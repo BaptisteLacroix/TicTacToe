87dbc527e1888e10335fef7aec605c67495976be
Baptiste Lacroix <baptiste.lacroix03@gmail.com>
2021-04-22 14:37:41 +0200

    Trying to resolve a bug (need to found how to resolve it)

--- Morpion.py
+++ Morpion.py
@@ -1,3 +1,4 @@
+import random
 import tkinter
 from tkinter import messagebox
 
@@ -15,25 +16,47 @@ class Game:
         self.clicked = True
         self.grid.create_cells()
         self.grid.display_grid()
+        self.botidiot = BotIdiot(self.grid.cells)
 
-    def button_click(self, btn):
-        """
-        method who check if the button pressed is empty, and print to the screen the character
-        :param btn: location of the pressed button
-        :return:
-        """
-        if btn["text"] == "" and self.clicked is True:  # if player has clicked 1 time
-            btn["text"] = 'O'
-            self.clicked = False  # Change the character for the next move
-            self.count += 1
-            self.check_if_win("O")  # check if the player 'O' win
-        elif btn["text"] == "" and self.clicked is False:  # the other player play
-            btn["text"] = 'X'
-            self.clicked = True  # Change the character for the next move
-            self.count += 1
-            self.check_if_win("X")  # check if the player 'X' win
+    def loop(self, x, y):
+
+        btn = self.botidiot.bot_is_playing()
+        print(btn)
+        print(self.grid.cells[btn[0]][btn[1]][btn["text"]] == "")
+
+        if self.grid.cells[x][y]["text"] == "" and self.clicked is True:  # if player has clicked 1 time
+            self.button_click(x, y)
+
+        elif self.grid.cells[btn[0]][btn[1]][btn["text"]] == "" and self.clicked is False:  # the other player play
+            self.button_click_bot(btn)
+
         else:
             messagebox.showerror(self.name, "Already Pressed")
+
+    def button_click(self, x, y):
+        """
+        method print to the screen the character
+        :param x: TODO
+        :param y: TODO
+        :return:
+        """
+
+        self.grid.cells[x][y]["text"] = 'O'
+        self.clicked = False  # Change the character for the next move
+        self.count += 1
+        self.check_if_win("O")  # check if the player 'O' win
+
+    def button_click_bot(self, btn):
+        """
+        method print to the screen the character
+        :param btn: TODO
+        :return:
+        """
+
+        self.grid.cells[btn[0]][btn[1]][btn["text"]] = 'X'
+        self.clicked = True  # Change the character for the next move
+        self.count += 1
+        self.check_if_win("X")  # check if the player 'X' win
 
     def check_if_win(self, player):
         """
@@ -41,30 +64,34 @@ class Game:
         :param player: is the current character
         :return:
         """
+
         self.check_row(player)
         self.check_column(player)
         self.check_diagonals(player)
         self.check_draw()
 
     def check_row(self, player):
+
         if self.grid.cells[0][0]["text"] == player and self.grid.cells[0][1]["text"] == player and \
                 self.grid.cells[0][2]["text"] == player or self.grid.cells[1][0]["text"] == player and \
                 self.grid.cells[1][1]["text"] == player and self.grid.cells[1][2]["text"] == player or \
                 self.grid.cells[2][0]["text"] == player and self.grid.cells[2][1]["text"] == player and self.grid.cells[
-                2][2]["text"] == player:
+            2][2]["text"] == player:
             self.winner = True
             messagebox.showinfo(self.name, player + "Win !")
             self.grid.disable_all_buttons()
 
     def check_column(self, player):
+
         if self.grid.cells[0][0]["text"] == player and self.grid.cells[1][1]["text"] == player and \
                 self.grid.cells[2][2]["text"] == player or self.grid.cells[0][2]["text"] == player and self.grid.cells[
-                1][1]["text"] == player and self.grid.cells[2][0]["text"] == player:
+            1][1]["text"] == player and self.grid.cells[2][0]["text"] == player:
             self.winner = True
             messagebox.showinfo(self.name, player + " Win ! ")
             self.grid.disable_all_buttons()
 
     def check_diagonals(self, player):
+
         if self.grid.cells[0][0]["text"] == player and self.grid.cells[1][0]["text"] == player and \
                 self.grid.cells[2][0]["text"] == player or self.grid.cells[0][1]["text"] == player and self.grid.cells[
             1][1]["text"] == player and self.grid.cells[2][1]["text"] == player or self.grid.cells[0][2]["text"] == \
@@ -74,6 +101,7 @@ class Game:
             self.grid.disable_all_buttons()
 
     def check_draw(self):
+
         if self.count >= 9:  # if all cells are filled
             messagebox.showinfo(self.name, " Nobody Won !")  # Alert player !
 
@@ -92,20 +120,22 @@ class Grid:
         Method who create the grid
         :return:
         """
+
         for x in range(self.height):
             self.cells.append([])
             for y in range(self.width):
                 btn = tkinter.Button(self.canvas, text="", width=20, height=10,
-                                     command=lambda: check_clicked(btn))
+                                     command=lambda: check_clicked(x, y))
+                print(x, y)
                 btn.grid(row=x, column=y)
                 self.cells[x].append(btn)
-        print(self.cells)
 
     def disable_all_buttons(self):
         """
         Method that disable all the buttons of the grid
         :return:
         """
+
         for x in range(self.height):
             for y in range(self.width):
                 self.cells[x][y].config(state=tkinter.DISABLED)
@@ -115,17 +145,25 @@ class Grid:
         Method who display the grid to the screen
         :return:
         """
+
         self.canvas.pack()
 
 
-class IA:
+class BotIdiot:
 
-    def __init__(self):
-        pass
+    def __init__(self, cells):
+        self.cells = cells
+
+    def bot_is_playing(self):
+        row = random.randint(0, 2)
+        column = random.randint(0, 2)
+        # print(row, column)
+        # print(self.cells[row][column])
+        return self.cells[row][column]
 
 
-def check_clicked(btn):
-    game.button_click(btn)
+def check_clicked(x, y):
+    game.loop(x, y)
 
 
 game = Game(3, 3)
