c95c0446b678b3b9ba84830f250f15940a617c3c
Baptiste Lacroix <baptiste.lacroix03@gmail.com>
2021-04-30 10:16:30 +0200

    Adding a stronger bot and simplifying the code

--- TicTacToe.py
+++ TicTacToe.py
@@ -18,13 +18,14 @@ class Game:
         self.name = "Tic tac toe"
         self.window = tkinter.Tk()
         self.window.title(self.name)  # title of the window
-        self.window.geometry("600x600")  # size of the window : 450x480
+        self.window.geometry("600x600")  # size of the window : 600x600
         self.my_menu = tkinter.Menu(self.window)
         self.window.config(menu=self.my_menu)
         options = tkinter.Menu(self.my_menu, tearoff=False)
         self.my_menu.add_cascade(label="Options", menu=options)
         options.add_command(label="Player VS Player", command=self.choice_player_vs_player)
-        options.add_command(label="Player VS Bot", command=self.choice_player_vs_bot)
+        options.add_command(label="Player VS Bot Idiot", command=self.choice_player_vs_bot_idiot)
+        options.add_command(label="Player VS Bot Hard", command=self.choice_player_vs_bot_hard)
         options.add_command(label="Restart Game", command=self.restart)
         self.height = height
         self.width = width
@@ -34,7 +35,7 @@ class Game:
         self.clicked = True
         self.grid.create_cells(lambda e: self.check_button(e, self.choice))
         self.grid.display_grid()
-        self.botidiot = BotIdiot(self.grid.cells)
+        self.botidiot = Bot(self.grid.cells)
 
     def restart(self):
         """
@@ -53,14 +54,21 @@ class Game:
         """
         self.choice = 1
 
-    def choice_player_vs_bot(self):
+    def choice_player_vs_bot_idiot(self):
         """
         Method used to update the value of choice
         :return:
         """
         self.choice = 2
 
-    def player_vs_bot(self, x, y):
+    def choice_player_vs_bot_hard(self):
+        """
+        Method used to update the value of choice
+        :return:
+        """
+        self.choice = 3
+
+    def player_vs_bot_idiot(self, x, y):
         """
         Method that allows a player to play against a bot
         :param x: line number
@@ -68,13 +76,35 @@ class Game:
         :return:
         """
 
-        btn_bot = self.botidiot.bot_is_playing(self.height, self.width)
-
-        if self.grid.cells[x][y]["text"] == "" and self.clicked is True and self.count <= 9:  # if player has clicked 1 time
+        if self.grid.cells[x][y]["text"] == "" and self.clicked is True and self.count <= 9:
+            # if player has clicked 1 time
             self.button_click_player1(x, y)
             self.grid.cells[x][y].config(state=tkinter.DISABLED)
 
-        elif self.grid.cells[btn_bot[0]][btn_bot[1]]["text"] == "" and self.clicked is False and self.count <= 9:  # the other player play
+        btn_bot = self.botidiot.bot_idiot_is_playing(self.height, self.width)
+
+        if self.grid.cells[btn_bot[0]][btn_bot[1]]["text"] == "" and self.clicked is False and self.count <= 9:
+            # the other player play
+            self.button_click_bot(btn_bot[0], btn_bot[1])
+            self.grid.cells[btn_bot[0]][btn_bot[1]].config(state=tkinter.DISABLED)
+
+    def player_vs_bot_hard(self, x, y):
+        """
+        Method that allows a player to play against a bot
+        :param x: line number
+        :param y: column number
+        :return:
+        """
+
+        if self.grid.cells[x][y]["text"] == "" and self.clicked is True and self.count <= 9:
+            # if player has clicked 1 time
+            self.button_click_player1(x, y)
+            self.grid.cells[x][y].config(state=tkinter.DISABLED)
+
+        btn_bot = self.botidiot.bot_hard_is_playing(self.height, self.width)
+
+        if self.grid.cells[btn_bot[0]][btn_bot[1]]["text"] == "" and self.clicked is False and self.count <= 9:
+            # the other player play
             self.button_click_bot(btn_bot[0], btn_bot[1])
             self.grid.cells[btn_bot[0]][btn_bot[1]].config(state=tkinter.DISABLED)
 
@@ -86,11 +116,13 @@ class Game:
         :return:
         """
 
-        if self.grid.cells[x][y]["text"] == "" and self.clicked is True and self.count <= 9:  # if player has clicked 1 time
+        if self.grid.cells[x][y]["text"] == "" and self.clicked is True and self.count <= 9:
+            # if player has clicked 1 time
             self.button_click_player1(x, y)
             self.grid.cells[x][y].config(state=tkinter.DISABLED)
 
-        elif self.grid.cells[x][y]["text"] == "" and self.clicked is False and self.count <= 9:  # the other player play
+        elif self.grid.cells[x][y]["text"] == "" and self.clicked is False and self.count <= 9:
+            # the other player play
             self.button_click_player2(x, y)
             self.grid.cells[x][y].config(state=tkinter.DISABLED)
 
@@ -105,16 +137,29 @@ class Game:
         # print(dir(event))
         # print(event.widget)
         if choice == 1:
-            for x in range(len(self.grid.cells)):
-                for y in range(len(self.grid.cells[x])):
-                    if event.widget == self.grid.cells[x][y] and event.widget["state"] == "normal":
-                        self.player_vs_player(x, y)
-
+            self.choice1(event)
         elif choice == 2:
-            for x in range(len(self.grid.cells)):
-                for y in range(len(self.grid.cells[x])):
-                    if event.widget == self.grid.cells[x][y] and event.widget["state"] == "normal":
-                        self.player_vs_bot(x, y)
+            self.choice2(event)
+        elif choice == 3:
+            self.choice3(event)
+
+    def choice1(self, event: tkinter.Event):
+        for x in range(len(self.grid.cells)):
+            for y in range(len(self.grid.cells[x])):
+                if event.widget == self.grid.cells[x][y] and event.widget["state"] == "normal":
+                    self.player_vs_player(x, y)
+
+    def choice2(self, event: tkinter.Event):
+        for x in range(len(self.grid.cells)):
+            for y in range(len(self.grid.cells[x])):
+                if event.widget == self.grid.cells[x][y] and event.widget["state"] == "normal":
+                    self.player_vs_bot_idiot(x, y)
+
+    def choice3(self, event: tkinter.Event):
+        for x in range(len(self.grid.cells)):
+            for y in range(len(self.grid.cells[x])):
+                if event.widget == self.grid.cells[x][y] and event.widget["state"] == "normal":
+                    self.player_vs_bot_hard(x, y)
 
     def button_click_player1(self, x, y):
         """
@@ -182,7 +227,7 @@ class Game:
             messagebox.showinfo(self.name, player + " Win !")
             self.grid.disable_all_buttons()
 
-    def check_column(self, player):
+    def check_diagonals(self, player):
         """
         method which checks for each column if the player wins
         :param player: Current player's pawn
@@ -195,7 +240,7 @@ class Game:
             messagebox.showinfo(self.name, player + " Win ! ")
             self.grid.disable_all_buttons()
 
-    def check_diagonals(self, player):
+    def check_column(self, player):
         """
         method which checks for each diagonal if the player wins
         :param player: Current player's pawn
@@ -271,7 +316,7 @@ class Grid:
         self.canvas.pack()
 
 
-class BotIdiot:
+class Bot:
 
     def __init__(self, cells):
         """
@@ -281,7 +326,7 @@ class BotIdiot:
         self.cells = cells
 
     @staticmethod
-    def bot_is_playing(x, y):
+    def bot_idiot_is_playing(x, y):
         """
         Method that randomly chooses row is column
         :param x: number of lines
@@ -291,6 +336,190 @@ class BotIdiot:
         row = random.randint(0, x - 1)
         column = random.randint(0, y - 1)
         return [row, column]
+
+    def bot_hard_is_playing(self, x, y):
+        """
+        TODO
+        :param x: TODO
+        :param y: TODO
+        :return: TODO
+        """
+
+        row = self.check_row()
+        column = self.check_column()
+        diagonal = self.check_diagonals()
+
+        if row is None and column is None and diagonal is None:
+            row = random.randint(0, x - 1)
+            column = random.randint(0, y - 1)
+            return [row, column]
+
+        elif row is not None:
+            return row
+
+        elif column is not None:
+            return column
+
+        elif diagonal is not None:
+            return diagonal
+
+    def check_row(self):
+        """
+        method which checks for each row if the player wins
+        :return: TODO
+        """
+        row0 = self.row0()
+        row1 = self.row1()
+        row2 = self.row2()
+        if row0 is True:
+            return row0[1]
+        elif row1 is True:
+            return row1[1]
+        elif row2 is True:
+            return row2[1]
+        else:
+            return None
+
+    def row0(self, player="X"):
+        """
+        TODO
+        :param player: Current player's pawn
+        :return: TODO
+        """
+        if self.cells[0][0]["text"] == player and self.cells[0][1]["text"] == player:
+            return True, [0, 2]
+        elif self.cells[0][0]["text"] == player and self.cells[0][2]["text"] == player:
+            return True, [0, 1]
+        elif self.cells[0][1]["text"] == player and self.cells[0][2]["text"] == player:
+            return True, [0, 0]
+
+    def row1(self, player="X"):
+        """
+        TODO
+        :param player: Current player's pawn
+        :return: TODO
+        """
+        if self.cells[1][0]["text"] == player and self.cells[1][1]["text"] == player:
+            return True, [1, 2]
+        elif self.cells[1][0]["text"] == player and self.cells[1][2]["text"] == player:
+            return True, [1, 1]
+        elif self.cells[1][1]["text"] == player and self.cells[1][2]["text"] == player:
+            return True, [1, 0]
+
+    def row2(self, player="X"):
+        """
+        TODO
+        :param player: Current player's pawn
+        :return: TODO
+        """
+        if self.cells[2][0]["text"] == player and self.cells[2][1]["text"] == player:
+            return True, [2, 2]
+        elif self.cells[2][0]["text"] == player and self.cells[2][2]["text"] == player:
+            return True, [2, 1]
+        elif self.cells[2][1]["text"] == player and self.cells[2][2]["text"] == player:
+            return True, [2, 0]
+
+    def check_column(self):
+        """
+        method which checks for each column if the player wins
+        :return: TODO
+        """
+
+        column0 = self.column0()
+        column1 = self.column1()
+        column2 = self.column2()
+        if column0 is True:
+            return column0[1]
+        elif column1 is True:
+            return column1[1]
+        elif column2 is True:
+            return column2[1]
+        else:
+            return None
+
+    def column0(self, player="X"):
+        """
+        TODO
+        :param player: Current player's pawn
+        :return: TODO
+        """
+
+        if self.cells[0][0]["text"] == player and self.cells[1][0]["text"] == player:
+            return True, [2, 0]
+        elif self.cells[0][0]["text"] == player and self.cells[2][0]["text"] == player:
+            return True, [1, 0]
+        elif self.cells[1][0]["text"] == player and self.cells[2][0]["text"] == player:
+            return True, [0, 0]
+
+    def column1(self, player="X"):
+        """
+        TODO
+        :param player: Current player's pawn
+        :return: TODO
+        """
+
+        if self.cells[0][1]["text"] == player and self.cells[1][1]["text"] == player:
+            return True, [2, 1]
+        elif self.cells[0][1]["text"] == player and self.cells[2][1]["text"] == player:
+            return True, [1, 1]
+        elif self.cells[1][1]["text"] == player and self.cells[2][1]["text"] == player:
+            return True, [0, 1]
+
+    def column2(self, player="X"):
+        """
+        TODO
+        :param player: Current player's pawn
+        :return: TODO
+        """
+
+        if self.cells[0][2]["text"] == player and self.cells[1][2]["text"] == player:
+            return True, [2, 2]
+        elif self.cells[0][2]["text"] == player and self.cells[2][2]["text"] == player:
+            return True, [1, 2]
+        elif self.cells[1][2]["text"] == player and self.cells[2][2]["text"] == player:
+            return True, [2, 0]
+
+    def check_diagonals(self):
+        """
+        method which checks for each column if the player wins
+        :return: TODO
+        """
+
+        diagonal0 = self.diagonal0()
+        diagonal1 = self.diagonal1()
+        if diagonal0 is True:
+            return diagonal0[1]
+        elif diagonal1 is True:
+            return diagonal1[1]
+        else:
+            return None
+
+    def diagonal0(self, player="X"):
+        """
+        TODO
+        :param player: Current player's pawn
+        :return: TODO
+        """
+
+        if self.cells[0][2]["text"] == player and self.cells[1][1]["text"] == player:
+            return True, [2, 0]
+        elif self.cells[0][2]["text"] == player and self.cells[2][0]["text"] == player:
+            return True, [1, 1]
+        elif self.cells[1][1]["text"] == player and self.cells[2][0]["text"] == player:
+            return True, [0, 2]
+
+    def diagonal1(self, player="X"):
+        """
+        TODO
+        :param player: Current player's pawn
+        :return: TODO
+        """
+        if self.cells[0][0]["text"] == player and self.cells[1][1]["text"] == player:
+            return True, [2, 2]
+        elif self.cells[0][0]["text"] == player and self.cells[2][2]["text"] == player:
+            return True, [1, 1]
+        elif self.cells[1][1]["text"] == player and self.cells[2][2]["text"] == player:
+            return True, [0, 0]
 
 
 game = Game(3, 3)
